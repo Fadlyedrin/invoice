8a2c51b2bbf0eac0bd9ae7f3451e9d0e269dba34
Fadly Edrin <fadlyedrin39@gmail.com>
2025-04-08 22:19:19 +0700

    update

--- resources/views/emails/receipt/status.blade.php
+++ resources/views/emails/receipt/status.blade.php
@@ -1,27 +1,30 @@
 @component('mail::message')
 # Notifikasi Perubahan Status Receipt
 
-Receipt untuk Invoice: **{{ $receipt->invoice->invoice_number }}**  
-Jumlah Dibayar: **Rp{{ number_format($receipt->amount_paid, 2) }}**  
-Metode Pembayaran: **{{ $receipt->payment_method }}**  
-Status Saat Ini: **{{ $receipt->status }}**
+Receipt untuk Invoice: *{{ $receipt->invoice->invoice_number }}*
+Jumlah Dibayar: *Rp{{ number_format($receipt->amount_paid, 2) }}*
+Metode Pembayaran: *{{ $receipt->payment_method }}*
+Status Saat Ini: *{{ $receipt->status }}*
 
 @isset($receipt->draft_data['approval_reason'])
-**Alasan Disetujui**: {{ $receipt->draft_data['approval_reason'] }}
+*Alasan Disetujui*: {{ $receipt->draft_data['approval_reason'] }}
 @endisset
 
 @isset($receipt->draft_data['rejection_reason'])
-**Alasan Ditolak**: {{ $receipt->draft_data['rejection_reason'] }}
+*Alasan Ditolak*: {{ $receipt->draft_data['rejection_reason'] }}
 @endisset
 
 @component('mail::button', ['url' => route('receipts.show', $receipt->id)])
 Lihat Detail Receipt
 @endcomponent
 
-@if ($qrCodeBase64)
-<p><strong>Scan QR Code untuk download PDF:</strong></p>
-{!! '<img src="' . $qrCodeBase64 . '" alt="QR Code" width="200">' !!}
+@if($receipt->status === 'Disetujui')
+---
+
+*Unduh PDF Receipt (Scan QR Code):*
+[<img src="{{ $qrCodeUrl }}" alt="QR Code" width="150">]({{ route('receipts.download', $receipt->id) }})
 @endif
 
 Terima kasih,<br>
+{{ config('app.name') }}
 @endcomponent
